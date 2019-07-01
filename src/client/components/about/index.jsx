e54aff61ece4588198664b1175de9fce44364a99
Dave Lee <davmlee@umich.edu>
2019-06-30 23:52:32 -0700

    update static about me

--- src/client/components/about/index.jsx
+++ src/client/components/about/index.jsx
@@ -8,12 +8,12 @@ class About extends Component {
         <p>
           Dave Lee <br></br>
           San Mateo, CA <br></br>
-          Software Engineer - Front End @ <a href="https://sendgrid.com">SendGrid</a>
+          Software Engineer @ <a href="https://sendgrid.com">Twilio SendGrid</a>
         </p>
         <div>
           <ul>
             <li>
-              <a href="mailto:dml777@gmail.com">dml777@gmail.com</a>
+              <a href="mailto:x.davelee@gmail.com">x.davelee@gmail.com</a>
             </li>
             <li>
               <a href="https://www.linkedin.com/in/davel33/">LinkedIn</a>
@@ -24,67 +24,11 @@ class About extends Component {
             <li>
               <a href="https://keybase.io/dlee">Keybase</a>
             </li>
+            <li>
+              <a href="https://www.yelp.com/user_details?userid=ChAYZfqSI8OHlFd4QlYyFA">Yelp</a>
+            </li>
           </ul>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-          Ut dapibus in eros sit amet vehicula.
-          Cras sagittis quis lacus et posuere.
-
-          Phasellus fringilla justo risus, nec ultricies ex venenatis ac.
-          Nunc sed risus eros.
-
-          Suspendisse vel orci non tellus eleifend consectetur.
-          Proin semper, justo non semper
-          elementum, est mi iaculis orci, sed condimentum elit mauris quis purus.
-          Donec consequat semper 
-          arcu, sed mollis neque egestas id.
-          Suspendisse in erat ut massa aliquet iaculis et eu tortor.
-
-          Aenean interdum ante in congue ultricies.
-          Quisque convallis metus vel feugiat semper.
-
-          In suscipit, neque eget lobortis egestas, metus nibh rhoncus diam, vel aliquam diam metus vel tortor.
-          Cras vitae dui leo.
-          Aliquam bibendum vestibulum purus.
-
-          Aenean a ante eget mi luctus congue.
-          Sed laoreet nunc non ipsum fringilla, id scelerisque ex vestibulum.
-          Suspendisse eget malesuada est.
-          Suspendisse id erat est.
-          Praesent nibh nisl, mattis et imperdiet sit amet, consectetur sit amet eros.
-          Nulla at felis vitae ipsum sollicitudin semper sit amet cursus nunc.
-          Morbi gravida semper leo, in blandit nunc sollicitudin ut.
-          Integer efficitur, justo ac semper aliquam, augue nulla blandit tellus, sit amet blandit mi purus non dui.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus.
-
-          Sed venenatis, turpis at tincidunt pretium, felis massa aliquet nulla, et tempor urna orci non lorem.
-          Mauris a elit vitae massa feugiat pretium eu nec justo.
-          Vivamus posuere scelerisque velit, sit amet mollis leo scelerisque ornare.
-          Aenean sem libero, pulvinar sed nulla id, convallis fringilla dui.
-          Duis porta mattis egestas.
-          Integer nec lacus tortor.
-          Proin est libero, volutpat faucibus sodales at, feugiat vel eros.
-          Quisque fermentum rhoncus lobortis.
-
-          Etiam suscipit viverra arcu in maximus.
-          Donec eget quam ipsum.
-          Suspendisse potenti.
-          Aliquam vehicula euismod orci a finibus.
-          In lectus dolor, tempus in volutpat id, euismod eu nisi.
-          Nullam convallis maximus hendrerit.
-          Vivamus eros quam, vulputate ac pharetra non, cursus eu ipsum.
-
-          Morbi suscipit nisi eget justo auctor, blandit hendrerit metus facilisis.
-          Vivamus vitae viverra tellus.
-          Integer in neque ac turpis ullamcorper ornare.
-          Integer vulputate, ante ut interdum aliquet, nunc quam pharetra justo, vel auctor risus lorem vel augue.
-          Maecenas viverra libero sit amet massa tempus luctus.
-          Pellentesque elementum, leo id facilisis tempor, risus lectus pharetra ante, sed iaculis justo felis sit amet metus.
-          Vestibulum eget arcu id leo ornare lacinia ut non tellus.
-          Suspendisse ante ligula, mattis sit amet diam viverra, tempus tristique sapien.
-        </p>
       </div>
     );
   }
