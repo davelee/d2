343304be8a674b05f5f39b18c9bc02714706f9ab
Dave Lee <davmlee@umich.edu>
2019-09-19 23:01:19 -0700

    dynamic meta tags..attempt

--- src/client/components/photography/photo/index.jsx
+++ src/client/components/photography/photo/index.jsx
@@ -27,12 +27,43 @@ class Photo extends React.Component {
     })
   }
 
+  getMeta = (url, caption) => {
+    return {
+      title: 'Dave Lee',
+      description: caption,
+      canonical: 'https://davelee.io',
+      meta: {
+        charSet: 'utf-8',
+        name: {
+          keywords: 'react,meta,document,html,tags'
+        },
+        itemProp: {
+          name: 'Dave Lee',
+          description: 'Dave Lee photography blog.',
+          image: url
+        },
+        property: {
+          'og:title': 'Dave Lee Photography',
+          'og:type': 'article',
+          'og:image': url,
+          'og:site_name': 'Dave Lee Photography',
+          'twitter:title': caption
+        }
+      },
+      auto: {
+        ograph: true
+      }
+    };
+  }
+
   render() {
     const { url, caption } = this.props;
     const { isImageLoaded } = this.state;
+    const meta = this.getMeta(url, caption);
     
     return (
       <div id={this.id} className="photo-wrap">
+        <DocumentMeta {...meta} />
         <Img className="photo" src={url} setLoaded={this.setImageLoaded} />
         <span className="photo-caption">
           {caption}
